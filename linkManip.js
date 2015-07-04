e8193ae9f8dca0cf7ad8aaffba0605130e8d09c5
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-07-04 19:12:50 +0200

    update

--- linkManip.js
+++ linkManip.js
@@ -3,7 +3,7 @@ var linkifyStr = require('linkifyjs/string');
 
 function linkifyYouTubeURLs(text) {
     var re = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig;
-    return text.replace(re, '<br><div><iframe width="440" height="315" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe></div><br>');
+    return text.replace(re, '<br><br><div><iframe width="440" height="315" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe></div><br>');
 }
 
 exports.linkifyText = function (text) {
