207c91bec1abc2cb2480caa5591b72cf8bcdc2e4
Alexandre Nguyen <alex.nr@hotmail.fr>
2015-12-25 18:08:15 -0500

    adapt trending.js for mongoose-paginate

--- trending.js
+++ trending.js
@@ -3,7 +3,7 @@ var gravatar = require('gravatar');
 
 exports.getPopularTrends = function(count, page, callback) {
 
-    model.ModelContainer.QuestionModel.paginate({is_deleted: false}, page, count, function (error, pageCount, paginatedResults, itemCount) {
+    model.ModelContainer.QuestionModel.paginate({is_deleted: false}, {page: page, limit: count}, function (error, pageCount, paginatedResults, itemCount) {
         if (error) {
             console.error(error);
             res.json(error);
